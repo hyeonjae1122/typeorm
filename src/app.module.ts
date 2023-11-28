45c7b8ea083bcb18e7470268a7a500f10cc4e647
hyeonjae <hyeonjae0913+1@gmail.com>
2023-11-28 23:22:07 +0900

    SingleInheritance in Typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,13 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
 import { StudentModel, TeacherModel } from './entity/person.entity';
+import {
+  AirplaneModel,
+  BookModel,
+  CarModel,
+  ComputerModel,
+  SingleBaseModel,
+} from './entity/inheritance.entity';
 
 @Module({
   imports: [
@@ -15,7 +22,16 @@ import { StudentModel, TeacherModel } from './entity/person.entity';
       port: 5432,
       username: 'postgres',
       password: 'postgres',
-      entities: [UserModel, StudentModel, TeacherModel],
+      entities: [
+        UserModel,
+        StudentModel,
+        TeacherModel,
+        BookModel,
+        CarModel,
+        ComputerModel,
+        AirplaneModel,
+        SingleBaseModel,
+      ],
       synchronize: true, //At production environmetn, this should be false
     }),
   ],
